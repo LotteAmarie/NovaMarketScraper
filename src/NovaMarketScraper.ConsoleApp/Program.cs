144130882f50d5d46d5b7bd4a6cee617ecaf95f0
Lotte <lottetheisen+github@gmail.com>
2018-05-06 19:51:15 -0500

    Initial Commit

---
+++ src/NovaMarketScraper.ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace NovaMarketScraper.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Item item = new Item 
+            {
+                Id = 210,
+                Name = "Test Object"
+            };
+
+            string json = JsonConvert.SerializeObject(item, Formatting.Indented);
+
+            System.Console.WriteLine(json);
+        }
+    }
+}
