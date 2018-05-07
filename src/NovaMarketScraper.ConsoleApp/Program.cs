a6031b40fc5d753894d62fb941f04dbe3325c203
Lotte <lottetheisen+github@gmail.com>
2018-05-06 23:45:36 -0500

    Implemented navigation to item all items list

--- src/NovaMarketScraper.ConsoleApp/Program.cs
+++ src/NovaMarketScraper.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -10,11 +11,16 @@ namespace NovaMarketScraper.ConsoleApp
         static void Main(string[] args)
         {
             var driver = new ChromeDriver(".");
+            var username = Console.ReadLine();
+            var password = Console.ReadLine();
+
+            NovaLogin(driver, username, password);
+            GenerateItems(driver);
 
             Console.ReadLine();
         }
 
-        static void NovaLogin(IWebDriver driver, string username, string password)
+        private static void NovaLogin(IWebDriver driver, string username, string password)
         {
             driver.Navigate().GoToUrl("https://www.novaragnarok.com/?module=vending");
 
@@ -23,6 +29,19 @@ namespace NovaMarketScraper.ConsoleApp
 
             var txtPassword = driver.FindElement(By.Name("password"));
             txtPassword.SendKeys(password);
+
+            var btnLogin = driver.FindElement(By.XPath("//*[@id=\"btnlogin\"]"));
+            btnLogin.Click();
+        }
+
+        private static List<Item> GenerateItems(IWebDriver driver)
+        {
+            driver.Navigate().GoToUrl("https://www.novaragnarok.com/?module=vending");
+
+            var btnSearch = driver.FindElement(By.XPath("//*[@id=\"mCSB_1_container\"]/div/div[3]/form/input[4]"));
+            btnSearch.Click();
+
+            return new List<Item>();
         }
     }
 }
