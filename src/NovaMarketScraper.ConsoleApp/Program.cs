6071fed756273f1f4ee91a733616229a993c6460
Lotte <lottetheisen+github@gmail.com>
2018-05-07 23:13:52 -0500

    Implemented item id scraping

--- src/NovaMarketScraper.ConsoleApp/Program.cs
+++ src/NovaMarketScraper.ConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Newtonsoft.Json;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.PhantomJS;
 
 namespace NovaMarketScraper.ConsoleApp
 {
@@ -15,7 +16,10 @@ namespace NovaMarketScraper.ConsoleApp
             var password = Console.ReadLine();
 
             NovaLogin(driver, username, password);
-            GenerateItems(driver);
+            var items = GenerateItems(driver);
+
+            string json = JsonConvert.SerializeObject(items, Formatting.Indented);
+            System.Console.WriteLine(json);
 
             Console.ReadLine();
         }
@@ -36,12 +40,31 @@ namespace NovaMarketScraper.ConsoleApp
 
         private static List<Item> GenerateItems(IWebDriver driver)
         {
-            driver.Navigate().GoToUrl("https://www.novaragnarok.com/?module=vending");
+            var items = new List<Item>();
+            for (int i = 1; i <= 3; i++)
+            {
+                driver.Navigate().GoToUrl($"https://www.novaragnarok.com/?module=item&action=index&name=&type=-1&p={i}");
 
-            var btnSearch = driver.FindElement(By.XPath("//*[@id=\"mCSB_1_container\"]/div/div[3]/form/input[4]"));
-            btnSearch.Click();
+                var table = driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div[2]/div[1]/div/div/table[2]"));
+                var rowCount = table.FindElements(By.TagName("tr")).Count;
 
-            return new List<Item>();
+                for (int j = 2; j < rowCount + 1; j++)
+                {
+                    var id = table.FindElement(By.XPath($"/html/body/div[2]/div[2]/div[2]/div[1]/div/div/table[2]/tbody/tr[{j}]/td[1]")).Text;
+                    var name = table.FindElement(By.XPath($"/html/body/div[2]/div[2]/div[2]/div[1]/div/div/table[2]/tbody/tr[{j}]/td[3]/a")).Text;
+
+                    var item = new Item
+                    {
+                        Id = Convert.ToInt32(id),
+                        Name = name
+                    };
+
+                    System.Console.WriteLine($"{id} {name}");
+                    items.Add(item);
+                }
+            }
+
+            return items;
         }
     }
 }
