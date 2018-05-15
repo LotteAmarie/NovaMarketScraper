ce17013dd7976c0b7105581afbd6ee1d2c5540f0
Lotte <lottetheisen+github@gmail.com>
2018-05-15 18:01:50 -0500

    Implemented code to load item DB into a in memory list of items.

--- src/NovaMarketScraper.ConsoleApp/Program.cs
+++ src/NovaMarketScraper.ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using Newtonsoft.Json;
 using OpenQA.Selenium;
@@ -12,7 +13,29 @@ namespace NovaMarketScraper.ConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.ReadLine();
+        }
 
+        public static IEnumerable<Item> LoadItems()
+        {
+            var itemStrings = File.ReadLines(@"items.txt");
+
+            var items = new List<Item>();
+            foreach (var itemString in itemStrings)
+            {
+                var strings = itemString.Split(',');
+
+                var item = new Item
+                {
+                    Id = Convert.ToInt32(strings[0]),
+                    Name = strings[1],
+                    Slots = Convert.ToInt32(strings[2])
+                };
+
+                items.Add(item);
+            }
+
+            return items;
         }
     }
 }
